2a03aa909422078fa3af6831bf48b1113178c59e
Kayla Pierson <kayla.pierson02@gmail.com>
2024-04-08 16:10:23 -0700

    foodpellet & board

---
+++ src/model/Board.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Board {
+	private int width;
+	private int height;
+	
+	public Board() {
+		
+	}
+}
---
+++ src/model/FoodPellet.java
@@ -0,0 +1,18 @@
+package model;
+
+public class FoodPellet {
+	public FoodPellet() {
+		
+	}
+	
+	public void spawn() {
+		// get random x y within width/height of board
+		//gc.strokeOval(., ., ., .}
+	}
+	
+	public boolean detectCollision() {
+		return false;
+		
+	}
+	
+}
