85c1ddd6b325153d0289cbe0268175dfb5a11357
Kayla Pierson <kayla.pierson02@gmail.com>
2024-04-08 20:35:41 -0700

    FoodPellet and Board updates

--- src/model/FoodPellet.java
+++ src/model/FoodPellet.java
@@ -1,18 +1,53 @@
 package model;
 
+import java.util.Random;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
 public class FoodPellet {
-	public FoodPellet() {
-		
+	private int SIZE = 2; // width & height of pellet - circle shape
+	private Color color;
+	private double x;
+	private double y;
+
+	private GraphicsContext gc;
+
+	public FoodPellet(Color color, int boardWidth, int boardHeight, GraphicsContext gc) {
+		this.color = color;
+		this.gc = gc;
+		spawn(boardWidth, boardHeight);
+	}
+
+	public void spawn(int boardWidth, int boardHeight) {
+		Random random = new Random();
+		x = random.nextDouble(boardWidth);
+		y = random.nextDouble(boardHeight);
+		draw();
 	}
 	
-	public void spawn() {
-		// get random x y within width/height of board
-		//gc.strokeOval(., ., ., .}
+	public void draw() {
+		gc.setStroke(color);
+		gc.fillOval(x, y, SIZE, SIZE);
 	}
-	
+
+	// assuming board background color is white for now
+	public void despawn() {
+		gc.setFill(Color.WHITE);
+		gc.fillOval(x, y, SIZE, SIZE);
+	}
+
+	// uses the coordinates of the snakes head to determine if
+	// pellet is eaten
 	public boolean detectCollision() {
 		return false;
-		
 	}
-	
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
 }
