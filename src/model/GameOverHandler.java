6a64469ca8741a35bf2fc9e73ce62fc0e2c086dc
Brendan Bamberg <uabbamberg@arizona.edu>
2024-04-08 21:54:37 -0700

    added simple stage for testing and added layout for GameOverHandler

--- src/model/GameOverHandler.java
+++ src/model/GameOverHandler.java
@@ -1,16 +1,76 @@
 package model;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
-public class GameOverHandler extends Application {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class GameOverHandler extends VBox {
     
-    public GameOverHandler() {
+    private ScoreManager scoreManager;
+    private Label gameOverLabel, scoreLabel, highScoreLabel;
+    private Button restart, quit;
     
+    public GameOverHandler(ScoreManager scoreManager) throws Exception {
+        this.scoreManager = scoreManager;
+        intializeComponents();
+        layoutGUI();
     }
     
-    @Override
-    public void start(Stage stage) {
-    
+    private void intializeComponents() throws Exception {
+        gameOverLabel = new Label("GAME OVER");
+        scoreLabel = new Label("Score: " + scoreManager.getCurrentScore());
+        highScoreLabel = new Label("High Score: " + scoreManager.getHighScore());
+        restart = new Button("Restart");
+        quit = new Button("Quit");
+        
+        FileInputStream fontInputStream;
+        try {
+            fontInputStream = new FileInputStream("resources/Emulogic-zrEw.ttf");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        
+        Font gameOverFont = Font.loadFont(fontInputStream, 36);
+        
+        try {
+            fontInputStream = new FileInputStream("resources/Emulogic-zrEw.ttf");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        
+        Font labelFont = Font.loadFont(fontInputStream, 15);
+        
+        gameOverLabel.setFont(gameOverFont);
+        scoreLabel.setFont(labelFont);
+        highScoreLabel.setFont(labelFont);
+        restart.setFont(labelFont);
+        quit.setFont(labelFont);
+        
+        restart.setOnAction(event -> {
+        
+        });
+        quit.setOnAction(event -> {
+        
+        });
+        
+        fontInputStream.close();
     }
+    
+    private void layoutGUI() {
+        this.getChildren().addAll(gameOverLabel, scoreLabel, highScoreLabel, restart, quit);
+        this.setAlignment(Pos.CENTER);
+        this.setSpacing(20);
+        
+        gameOverLabel.setAlignment(Pos.CENTER);
+        scoreLabel.setAlignment(Pos.CENTER);
+        highScoreLabel.setAlignment(Pos.CENTER);
+        restart.setAlignment(Pos.CENTER);
+        quit.setAlignment(Pos.CENTER);
+    }
+    
 }
