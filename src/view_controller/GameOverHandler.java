6bef03fb79ea066bd049e0983f796715e8486f42
sameeka03 <148290755+sameeka03@users.noreply.github.com>
2024-04-25 17:45:03 -0700

    Update GameOverHandler.java

--- src/view_controller/GameOverHandler.java
+++ src/view_controller/GameOverHandler.java
@@ -1,70 +1,95 @@
 package view_controller;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
-import javafx.stage.Stage;
+import model.SnakeGame;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * A Scene that shows the Game Over state of a game of Snake
+ *
  * @author Brendan Bamberg
  */
 
 public class GameOverHandler extends VBox {
-    private SnakeGUI snakeGUI;
-    private Scene gameOverScene;
+    
+    private SnakeGame theGame;
     private Label gameOverLabel, scoreLabel, highScoreLabel;
     private Button restart, quit;
-
-    public GameOverHandler(SnakeGUI snakeGUI, Stage stage) {
-        this.snakeGUI = snakeGUI;
-        initializeComponents();
+    
+    /**
+     * Creates a new GameOverHandler object
+     *
+     * @param theGame A SnakeGame object that has information about the current game state
+     */
+    public GameOverHandler(SnakeGame theGame) {
+        this.theGame = theGame;
+        intializeComponents();
         layoutGUI();
-        gameOverScene = new Scene(this, 300, 200); 
-        stage.setScene(gameOverScene);
-        stage.show();
     }
-
-    private void initializeComponents() {
+    
+    /**
+     * Initializes all components of the Scene with custom fonts
+     */
+    private void intializeComponents() {
         gameOverLabel = new Label("GAME OVER");
-        scoreLabel = new Label("Score: " + snakeGUI.getSnakeGame().getScoreManager().getCurrentScore());
-        highScoreLabel = new Label("High Score: " + snakeGUI.getSnakeGame().getScoreManager().getHighScore());
+        scoreLabel = new Label("Score: " + theGame.getScoreManager().getCurrentScore());
+        highScoreLabel = new Label("High Score: " + theGame.getScoreManager().getHighScore());
         restart = new Button("Restart");
         quit = new Button("Quit");
-
-        Font gameOverFont = loadFont("resources/Emulogic-zrEw.ttf", 36);
-        Font labelFont = loadFont("resources/Emulogic-zrEw.ttf", 15);
-
+        
+        // Create a new custom font for the Game Over text
+        FileInputStream fontInputStream;
+        try {
+            fontInputStream = new FileInputStream("resources/Emulogic-zrEw.ttf");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        Font gameOverFont = Font.loadFont(fontInputStream, 36);
+        
+        // Create a new custom font for the other text
+        try {
+            fontInputStream = new FileInputStream("resources/Emulogic-zrEw.ttf");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        
+        Font labelFont = Font.loadFont(fontInputStream, 15);
+        
         gameOverLabel.setFont(gameOverFont);
         scoreLabel.setFont(labelFont);
         highScoreLabel.setFont(labelFont);
         restart.setFont(labelFont);
         quit.setFont(labelFont);
-
-        restart.setOnAction(event -> snakeGUI.startGame()); // Assuming startGame resets and starts the game
-        quit.setOnAction(event -> System.exit(0));
-
+        
+        restart.setOnAction(event -> {
+        
+        });
+        quit.setOnAction(event -> {
+        
+        });
+        
+        try {
+            fontInputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    /**
+     * Creates the layout of the Scene
+     */
+    private void layoutGUI() {
         this.getChildren().addAll(gameOverLabel, scoreLabel, highScoreLabel, restart, quit);
         this.setAlignment(Pos.CENTER);
         this.setSpacing(20);
-    }
-
-    private Font loadFont(String path, double size) {
-        try (FileInputStream fontInputStream = new FileInputStream(path)) {
-            return Font.loadFont(fontInputStream, size);
-        } catch (IOException e) {
-            System.err.println("Failed to load font: " + path);
-            return Font.font("System", size); // Default to a system font on error
-        }
-    }
-
-    private void layoutGUI() {
+        
         gameOverLabel.setAlignment(Pos.CENTER);
         scoreLabel.setAlignment(Pos.CENTER);
         highScoreLabel.setAlignment(Pos.CENTER);
